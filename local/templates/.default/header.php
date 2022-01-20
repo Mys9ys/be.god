739493735be387abf457e6ab4ef733eb1db25aaf
mys9ys <rjbexa@yandex.ru>
2022-01-20 10:31:54 +0300

    +menu

--- local/templates/.default/header.php
+++ local/templates/.default/header.php
@@ -11,6 +11,21 @@
     <meta charset="UTF-8">
     <title>Божество</title>
 </head>
+
 <div class="container">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Настройки</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Страна</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Город</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" >Компании</a>
+        </li>
+    </ul>
     <h1>Header</h1>
 </div>
