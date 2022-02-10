08b840fc200306e22bfe86f65ec99b0ede8bc609
mys9ys <rjbexa@yandex.ru>
2022-02-10 09:51:16 +0300

    commit for merge

--- company/index.php
+++ company/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('../local/templates/.default/header.php');
+require_once('local/templates/.default/header.php');
 
 require_once('../local/templates/components/company_comp/templates/.default/template.php');
 
-require_once ('../local/templates/.default/footer.php');
+require_once ('local/templates/.default/footer.php');
